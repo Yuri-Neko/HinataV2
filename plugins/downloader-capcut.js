3f14d5f7e27a5729f852ce61e8ecc7965d79b638
AyGemuy <91945119+AyGemuy@users.noreply.github.com>
2023-06-24 18:38:57 +0800

    Add files via upload

--- plugins/downloader-capcut.js
+++ plugins/downloader-capcut.js
@@ -1,6 +1,6 @@
-import cheerio from 'cheerio';
-import fetch from 'node-fetch';
-
+import cheerio from "cheerio";
+import fetch from "node-fetch";
+import got from "got";
 let handler = async (m, {
     conn,
     args,
@@ -57,7 +57,7 @@ let handler = async (m, {
 🎥 *original video URL:* ${item.originalVideoUrl}
 `
                 
-                await conn.sendFile(m.chat, item.originalVideoUrl || logo, '', cap || 'Tidak diketahui', m)
+                await conn.sendFile(m.chat, item.originalVideoUrl || logo, "", cap || "Tidak diketahui", m)
             } catch (e) {
                 await m.reply(eror)
             }
@@ -74,18 +74,18 @@ async function downloadCapcut(Url) {
 	try {
 		const token = Url.match(/\d+/)[0];
 		const response = await fetch(`https://ssscapcut.com/api/download/${token}`, {
-			method: 'GET',
+			method: "GET",
 			headers: {
-				'Accept': '/',
-				'User-Agent': 'Mozilla/5.0 (Linux; Android 13; CPH2217 Build/TP1A.220905.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/110.0.5481.153 Mobile Safari/537.36',
-				'X-Requested-With': 'acr.browser.barebones',
-				'Sec-Fetch-Site': 'same-origin',
-				'Sec-Fetch-Mode': 'cors',
-				'Sec-Fetch-Dest': 'empty',
-				'Referer': 'https://ssscapcut.com/',
-				'Accept-Encoding': 'gzip, deflate',
-				'Accept-Language': 'id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7',
-				'Cookie': 'sign=2cbe441f7f5f4bdb8e99907172f65a42; device-time=1685437999515'
+				"Accept": "/",
+				"User-Agent": "Mozilla/5.0 (Linux; Android 13; CPH2217 Build/TP1A.220905.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/110.0.5481.153 Mobile Safari/537.36",
+				"X-Requested-With": "acr.browser.barebones",
+				"Sec-Fetch-Site": "same-origin",
+				"Sec-Fetch-Mode": "cors",
+				"Sec-Fetch-Dest": "empty",
+				"Referer": "https://ssscapcut.com/",
+				"Accept-Encoding": "gzip, deflate",
+				"Accept-Language": "id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7",
+				"Cookie": "sign=2cbe441f7f5f4bdb8e99907172f65a42; device-time=1685437999515"
 			}
 		});
 		const data = await response.json();
@@ -98,19 +98,19 @@ async function downloadCapcut(Url) {
 
 async function searchTemplates(s) {
   try {
-    const response = await fetch('https://capcut-templates.com/?s=' + s);
-    const html = await response.text();
+    const response = await got("https://capcut-templates.com/?s=" + s);
+    const html = response.body;
     const $ = cheerio.load(html);
-    const elements = $('main#main div.ct-container section div.entries article');
+    const elements = $("main#main div.ct-container section div.entries article");
 
     const detailPromises = elements.map(async (index, element) => {
-      const link = $(element).find('a.ct-image-container').attr('href');
+      const link = $(element).find("a.ct-image-container").attr("href");
       const detail = await detailTemplates(link);
-      const imageSrc = $(element).find('img').attr('src');
-      const title = $(element).find('h2.entry-title a').text().trim();
+      const imageSrc = $(element).find("img").attr("src");
+      const title = $(element).find("h2.entry-title a").text().trim();
 
       return {
-        id: $(element).attr('id'),
+        id: $(element).attr("id"),
         link,
         detail,
         imageSrc,
@@ -127,20 +127,20 @@ async function searchTemplates(s) {
 
 async function detailTemplates(link) {
   try {
-    const response = await fetch(link);
-    const html = await response.text();
+    const response = await got(link);
+    const html = response.body;
     const $ = cheerio.load(html);
-    const elements = $('main#main div.ct-container-full article');
+    const elements = $("main#main div.ct-container-full article");
 
     return elements.map((index, element) => ({
-      id: $(element).attr('id'),
-      time: $('main#main').find('time.ct-meta-element-date').text().trim(),
-      template: $(element).find('.wp-block-buttons .wp-block-button a').attr('data-template-id'),
-      link: $(element).find('a.wp-block-button__link').attr('href'),
-      imageSrc: $(element).find('video').attr('poster'),
-      title: $(element).find('h2').text().trim(),
-      videoSrc: $(element).find('video source').attr('src'),
-      description: $(element).find('.entry-content p').text().trim()
+      id: $(element).attr("id"),
+      time: $("main#main").find("time.ct-meta-element-date").text().trim(),
+      template: $(element).find(".wp-block-buttons .wp-block-button a").attr("data-template-id"),
+      link: $(element).find("a.wp-block-button__link").attr("href"),
+      imageSrc: $(element).find("video").attr("poster"),
+      title: $(element).find("h2").text().trim(),
+      videoSrc: $(element).find("video source").attr("src"),
+      description: $(element).find(".entry-content p").text().trim()
     })).get();
   } catch (error) {
     console.log(error);
